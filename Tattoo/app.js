a301bad079f1210c6efeec6225fd14456e893985
githron <allewnor@gmail.com>
2022-12-30 11:46:31 +0000

    init

--- Tattoo/app.js
+++ Tattoo/app.js
@@ -4,12 +4,17 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0]
 const artistButton = document.getElementsByClassName('artist-btn')[0]
 const homeButton = document.getElementsByClassName('home-btn')[0]
 const aboutButton = document.getElementsByClassName('about-btn')[0]
+const galleryButton = document.getElementsByClassName('gallery-btn')[0]
+
+
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-
+galleryButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
 
 artistButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
