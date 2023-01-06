55c8249b964d8c1143c5e535c8bcd72271254f7c
githron <allewnor@gmail.com>
2023-01-06 13:07:11 +0000

    init

--- Tattoo/app.js
+++ Tattoo/app.js
@@ -1,38 +1,4 @@
-const toggleButton = document.getElementsByClassName('toggle-btn')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
-const artistButton = document.getElementsByClassName('artist-btn')[0]
-const homeButton = document.getElementsByClassName('home-btn')[0]
-const aboutButton = document.getElementsByClassName('about-btn')[0]
-const galleryButton = document.getElementsByClassName('gallery-btn')[0]
-const servicesButton = document.getElementsByClassName('services-btn')[0]
 
-
-
-
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
-galleryButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
-servicesButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-
-artistButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
-homeButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
-aboutButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
 
 var swiper = new Swiper(".mySwiper", {
     loop: true,
