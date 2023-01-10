d4a1bce56fa08dce2f7c36e95dbb6761be43bec7
githron <allewnor@gmail.com>
2023-01-10 11:30:10 +0000

    init

--- Tattoo/app.js
+++ Tattoo/app.js
@@ -16,7 +16,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 3000,
+      delay: 5000,
       disableOnInteraction: false,
     },
     pagination: {
