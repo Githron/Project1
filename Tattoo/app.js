29c5590d37c1e29d1dd818af61469255e30375ff
githron <allewnor@gmail.com>
2023-01-14 21:26:13 +0000

    init

--- Tattoo/app.js
+++ Tattoo/app.js
@@ -14,23 +14,22 @@
 //   });
 
 //animate on preload
-  window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     var preloader = document.getElementById("preloader");
-    const xelement = document.querySelector('.bakimg');
-    const zelement = document.querySelector('.title-name');
-    const belement = document.querySelector('.fadeX');
-    setTimeout(function() {
-      preloader.style.animationPlayState = 'running';
-      xelement.style.animationPlayState = 'running';
-      zelement.style.animationPlayState = 'running';
-      belement.style.animationPlayState = 'running';
-      setTimeout(() => {
-          preloader.style.display = "none";
-      }, 1000);
+    const xelement = document.querySelector(".bakimg");
+    const zelement = document.querySelector(".title-name");
+    const belement = document.querySelector(".fadeX");
+    setTimeout(function () {
+        preloader.style.animationPlayState = "running";
+        xelement.style.animationPlayState = "running";
+        zelement.style.animationPlayState = "running";
+        belement.style.animationPlayState = "running";
+        setTimeout(() => {
+            preloader.style.display = "none";
+        }, 1000);
     }, 0); // milliseconds
-  });
-  //end
-  
+});
+//end
 
 // Swiper animate Auto
 var swiper = new Swiper(".mySwiper", {
@@ -38,59 +37,61 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
+        delay: 5000,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
-  //end
-
-
-  // Oberver Intersection animate when scrolling
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio >= 0.25) {
-        // The element is visible on at least 25% of the view
-        entry.target.style.animationPlayState = "running";
-      } else {
-        entry.target.style.animationPlayState = "paused";
-      }
-    });
-  }, { threshold: [0.25] });
-  const artistImgElements = document.querySelectorAll(".artist-img");
-  artistImgElements.forEach(element => {
-    observer.observe(element);
-  });
+});
 //end
-  
 
-
+// Oberver Intersection animate when scrolling
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio >= 0.25) {
+                // The element is visible on at least 25% of the view
+                entry.target.style.animationPlayState = "running";
+            } else {
+                entry.target.style.animationPlayState = "paused";
+            }
+        });
+    },
+    { threshold: [0.25] }
+);
+const artistImgElements = document.querySelectorAll(".artist-img");
+artistImgElements.forEach((element) => {
+    observer.observe(element);
+});
+//end
 
 // Toggle Menu
-  function toggle() {
+function toggle() {
     var element = document.getElementById("myDiv");
-    if (element.style.opacity == 0) {
-      element.style.display = 'flex';
-      element.style.opacity = 0;
-      setTimeout(function() {
-        element.style.opacity = .95;
-      }, 10);
-    } else {
 
-      setTimeout(function() {
-        element.style.display = 'none';
-      }, 1000);
-      element.style.opacity = 0;
-    }
-  }         //end
+    // var element = document.getElementById("slideout");
+    element.classList.toggle("open");
 
+    //   if (element.style.opacity == 0) {
+    //     element.style.display = 'flex';
+    //     element.style.opacity = 0;
+    //     setTimeout(function() {
+    //       element.style.opacity = .95;
+    //     }, 10);
+    //   } else {
+
+    //     setTimeout(function() {
+    //       element.style.display = 'none';
+    //     }, 1000);
+    //     element.style.opacity = 0;
+    //   }
+} //end
 
 //trial
 // const button = document.getElementById("myDiv");
