d6bef330db7d9dd3a4d0cbb559f7db963ef5fcab
githron <allewnor@gmail.com>
2023-01-17 09:05:50 +0000

    init

--- myWeb/page about/about.js
+++ myWeb/page about/about.js
@@ -1,5 +1,56 @@
 
 
+// .moreDetail {
+//     font-weight: 350;
+//     line-height: 1.75;
+//     margin-top: 35px;
+//     margin-bottom: 75px;
+//     transition: all 1s ease-out;
+//     opacity: 0;
+//     transform: translateY(-70%);
+//     visibility: hidden;
+//     display: none;
+// }
+
+// .moreDetail.open {
+//     transition: all 1s ease-out;
+//     transform: translateY(0);
+//     visibility: visible;
+//     opacity: 1;
+// }
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var moreDetail = document.querySelector(".moreDetail");
+//     var hideX = document.querySelector(".hideX");
+//     var clickMore = document.querySelector(".clickMore");
+
+//     hideX.addEventListener("click", function() {
+//         if (moreDetail.classList.contains("open")) {
+//             moreDetail.style.opacity = "0";
+//             moreDetail.classList.remove("open");
+//             setTimeout(function() {
+//                 moreDetail.style.display = "none";
+//                 clickMore.style.display = "block";
+//             }, 1000);
+//         } else {
+//             moreDetail.style.display = "block";
+//             moreDetail.classList.add("open");
+//             moreDetail.style.opacity = "1";
+//             clickMore.style.display = "none";
+//         }
+//     });
+// });
+
+
+
+
+
+
+
+
+
 //jQuery slide in/out up/down
 $(document).ready(function () {
     $(".hideX").click(function () {
