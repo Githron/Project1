4b4fcf7e6e39381bb0566fd6f1ca03a06158bb17
githron <allewnor@gmail.com>
2023-02-18 12:06:22 +0000

    init

---
+++ quiz-app frontEnd/quiz.js
@@ -0,0 +1,125 @@
+let quiz_rules = [
+    "You Have Only 20 Seconds For Each Question.",
+    "Once You Select Any Answer.It Can't Be Undone",
+    "You'll Get Points On The Basic Of Your Correct Answers."
+];
+
+
+let quiz_questions = [
+    // question 1
+    {
+        // question
+        question: "3 x 2 = ?",
+
+        // options
+        options : [
+            "4",
+            "5",
+            "6",
+            "7",
+        ],
+
+        // correct answer
+        answer: 3,
+    },
+    
+    {
+        // write yout question here
+        question: "4 x 5 = ?",
+    
+        options: [
+            "15",
+            "18",
+            "20",
+            "25",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "6 x 3 = ?",
+    
+        options: [
+            "12",
+            "15",
+            "18",
+            "21",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "2 x 8 = ?",
+    
+        options: [
+            "10",
+            "12",
+            "14",
+            "16",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 4,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "5 x 7 = ?",
+    
+        options: [
+            "30",
+            "35",
+            "40",
+            "45",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "9 x 4 = ?",
+    
+        options: [
+            "27",
+            "32",
+            "36",
+            "40",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "8 x 2 = ?",
+    
+        options: [
+            "10",
+            "14",
+            "16",
+            "20",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "3 x 9 = ?",
+    
+        options: [
+            "24",
+            "25",
+            "26",
+            "27",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 4,
+    //    selected_answer:null
+    },
+    
+];
